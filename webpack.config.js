c235e25b904d4a3d6dd310896a3e1fc04d96a1b7
DaviJP7 <davi.dejesush2o@hotmail.com>
2022-12-10 21:07:11 -0300

    include webpack

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+            test: /\.js$/i, include: path.resolve(__dirname, 'src'), use: {
+                loader: 'babel-loader', options: {
+                    presets: ['@babel/preset-env'],
+                },
+            },
+        }, {
+            test: /\.css$/i,
+            use: [
+                'style-loader',
+                'css-loader',
+                'postcss-loader'
+            ],
+        },],
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        })
+    ]
+}
